33df28cad011670c4c1af66a571c9c2dd4b92ea9
Jonas Lilletvedt <jonas.lilletved@icloud.com>
2025-10-03 22:19:31 +0200

    linear regression

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -11,6 +11,7 @@ use crate::model_base::{Labels, Mode, ModelBase};
 #[pyclass]
 pub struct MyRustLinearRegression {
     iterations: usize,
+    learning_rate: f64,
     base: ModelBase,
     weights: Array1<f64>,
     bias: f64,
@@ -19,12 +20,13 @@ pub struct MyRustLinearRegression {
 #[pymethods]
 impl MyRustLinearRegression {
     #[new]
-    fn new(iterations: usize, mode: Mode) -> Self {
+    fn new(iterations: usize, learning_rate: f64, mode: Mode) -> Self {
         MyRustLinearRegression {
             iterations: (iterations),
+            learning_rate: (learning_rate),
             base: ModelBase::new(mode),
             weights: Array1::zeros(1), // Not worth the trouble to wrap in option, just start size to 1, not able to missuse due to it checking if fit before predict
-            bias: f64 = 0.0,
+            bias: 0.0,
         }
     }
 
@@ -33,7 +35,11 @@ impl MyRustLinearRegression {
         self.base.X = Some(X.as_array().to_owned());
         self.base.y = Some(labels);
         self.weights =
-            Array1::<f64>::zeros(self.base.X.as_ref().expect("Model not fitted").nrows());
+            Array1::<f64>::zeros(self.base.X.as_ref().expect("Model not fitted").ncols());
+
+        for i in 0..self.iterations {
+            self.gradient_descent();
+        }
         Ok(())
     }
 
@@ -50,7 +56,7 @@ impl MyRustLinearRegression {
 
         match y_train {
             Labels::Float(y_train_float) => {
-                let res: Array1<f64> = todo!();
+                let res: Array1<f64> = make_predictions(X_train, &self.weights, &self.bias);
                 let np_res = res.into_pyarray(py);
                 Ok(np_res.into())
             }
@@ -64,15 +70,53 @@ impl MyRustLinearRegression {
     }
 }
 
-fn make_loss_predictions(
-    X_train: &Array2<f64>,
-    y_train: &Array1<f64>,
-    weights: &Array1<f64>,
-    bias: &f64,
-) -> Array1<Mode> {
-    let y_loss_predictions: Array1<f64> = Array1::zeros(y_train.len());
-    for (i, (row_train, y_row)) in X_train.into_iter().zip(y_train.iter()).enumerate() {
-        let row_loss = (*y_row - (weights * *row_train + *bias)).pow2();
+impl MyRustLinearRegression {
+    fn gradient_descent(&mut self) {
+        let X_train = self
+            .base
+            .X
+            .as_ref()
+            .expect("Code Error [fn gradient_descent] -- self.base.X could not unwrap_failed");
+        let y_train = self
+            .base
+            .y
+            .as_ref()
+            .expect("Code Error [fn gradient_descent] -- self.base.y could not unwrap_failed");
+
+        let y_train_float = match y_train {
+            Labels::Float(y) => y,
+            Labels::Int(_) => return,
+        };
+
+        // predictions
+        let predictions = make_predictions(X_train, &self.weights, &self.bias);
+
+        // errors
+        let errors = &predictions - y_train_float;
+
+        // gradients
+        let weight_loss_derivatives = calculate_weight_loss_derivative(&errors, X_train);
+
+        // update weights and bias
+        self.weights -= &(weight_loss_derivatives * self.learning_rate);
+
+        let bias_derivative = errors.mean().unwrap();
+        self.bias -= bias_derivative * self.learning_rate;
     }
-    todo!()
+}
+
+fn make_predictions(X_train: &Array2<f64>, weights: &Array1<f64>, bias: &f64) -> Array1<f64> {
+    X_train.dot(weights) + *bias
+}
+
+fn calculate_weight_loss_derivative(errors: &Array1<f64>, X_train: &Array2<f64>) -> Array1<f64> {
+    let m = X_train.nrows();
+
+    let weights_derivatives = X_train
+        .columns()
+        .into_iter()
+        .map(|feat| feat.dot(errors) / m as f64)
+        .collect::<Array1<f64>>();
+
+    weights_derivatives
 }
